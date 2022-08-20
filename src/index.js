eb49a892b488604d934a709aa7d9519559da8072
Alina Guscha <alialinaa19@gmail.com>
2022-08-20 22:33:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
-const input = document.querySelector("input#search-box");
-console.log(input)
+const refs = {
+    input:  document.querySelector("input#search-box"),
+    countryInfo: document.querySelector(".country-info"),
+    countryList: document.querySelector(".country-list"),
+};
+
+refs.input.addEventListener("submit", onSearch);
+
+function onSearch(e) {
+    const searchQuery = e.currentTarget.elements.query.value;
+    console.log(searchQuery)
+}
