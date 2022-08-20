1874b20dfebac894675c8ff4530e019576bc3419
Alialinaa19 <96882681+Alialinaa19@users.noreply.github.com>
2022-08-20 19:39:21 +0000

    Deploying to gh-pages from @ Alialinaa19/goit-js-hw-10@d8f18ae399803a1c690ecf142fb86a91b013e30c 🚀

--- index.bfb6b363.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector("input#search-box"),document.querySelector(".country-info"),document.querySelector(".country-list")][0].addEventListener("input",(function(e){var t=e.currentTarget.elements.query.value;console.log(t)}));
-//# sourceMappingURL=index.bfb6b363.js.map
---
+++ index.ee9dddcb.js
@@ -0,0 +1,2 @@
+[document.querySelector("input#search-box"),document.querySelector(".country-info"),document.querySelector(".country-list")][0].addEventListener("input",(function(e){console.log(e)}));
+//# sourceMappingURL=index.ee9dddcb.js.map
