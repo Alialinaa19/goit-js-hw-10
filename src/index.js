d021c5923f51ef1e9d109c8c1c10a4b9f6453347
Alina Guscha <alialinaa19@gmail.com>
2022-08-21 02:09:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,10 +9,7 @@ const refs = {
     countryList: document.querySelector(".country-list"),
 };
 
-refs.input.addEventListener("input", onSearch);
-
-function onSearch(e) {
-    debounce (e => {
+refs.input.addEventListener("input", debounce (e => {
     const countryName = e.currentTarget.value;
     fetch(`https://restcountries.com/v2/name/${countryName}`)
     .then((response) => response.json())
@@ -38,8 +35,37 @@ function onSearch(e) {
         
     }
     )
-}, DEBOUNCE_DELAY)
-}
+}, DEBOUNCE_DELAY));
+
+// function onSearch(e) {
+//     debounce (e => {
+//     const countryName = e.currentTarget.value;
+//     fetch(`https://restcountries.com/v2/name/${countryName}`)
+//     .then((response) => response.json())
+//     .then((data) => {console.log(data)
+//     if(data.length > 10) {
+//         Notiflix.Notify.info(
+//             "Too many matches found. Please enter a more specific name."
+//           );
+//     }
+//     else if(data.length > 1 || data.length < 9) {
+//         insertContent(data)
+//     }
+//     else {
+
+//     }
+//     })
+//     .catch((error) =>{
+//         if (error.status === 404) {
+//             Notiflix.Notify.failure(
+//                 "Oops, there is no country with that name"
+//               );
+//         }
+        
+//     }
+//     )
+// }, DEBOUNCE_DELAY)
+// }
 
 function createLi(item) {
 `<li>
