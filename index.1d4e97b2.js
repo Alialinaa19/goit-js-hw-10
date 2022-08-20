1a240ad1bae073ab403580806072230bc228ce7f
Alialinaa19 <96882681+Alialinaa19@users.noreply.github.com>
2022-08-20 20:26:32 +0000

    Deploying to gh-pages from @ Alialinaa19/goit-js-hw-10@ea1c42a1dd5ab3f36cbf3930c7ece920f92b2aa2 🚀

--- index.1d4e97b2.js
+++
@@ -1,2 +0,0 @@
-({input:document.querySelector("input#search-box"),countryInfo:document.querySelector(".country-info"),countryList:document.querySelector(".country-list")}).input.addEventListener("input",(function(t){const e=t.currentTarget.value;fetch(`https://restcountries.com/v2/name/${e}`).then((t=>t.json())).then((t=>console.log(t)))}));
-//# sourceMappingURL=index.1d4e97b2.js.map
--- index.bdee6289.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector("input#search-box"),document.querySelector(".country-info"),document.querySelector(".country-list")][0].addEventListener("input",(function(e){var t=e.currentTarget.value;fetch("https://restcountries.com/v2/name/".concat(t)).then((function(e){return e.json()})).then((function(e){return console.log(e)}))}));
-//# sourceMappingURL=index.bdee6289.js.map
