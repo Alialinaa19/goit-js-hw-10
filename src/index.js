5583ff606d125e58d963b547ad6d2acb54758653
Alina Guscha <alialinaa19@gmail.com>
2022-08-21 21:55:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,58 +1,82 @@
-import './css/styles.css';
-import Notiflix from 'notiflix';
-import debounce from 'lodash';
+// import './css/styles.css';
+// import Notiflix from 'notiflix';
+// import debounce from 'lodash.debounce';
+// import { fetchCountries } from './js/fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
-const refs = {
-    input:  document.querySelector("input#search-box"),
-    countryInfo: document.querySelector(".country-info"),
-    countryList: document.querySelector(".country-list"),
-};
+// const DEBOUNCE_DELAY = 300;
+// const refs = {
+//     input:  document.querySelector("input#search-box"),
+//     countryInfo: document.querySelector(".country-info"),
+//     countryList: document.querySelector(".country-list"),
+// };
 
-refs.input.addEventListener("input", onSearch)
+// refs.input.addEventListener("input", onSearch)
 
-function onSearch(e) {
-    cleanHtml();
-    const countryName = e.currentTarget.value.trim();
-    fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=,name,capital,population,flags,languages`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-    if(data.length > 10) {
-        Notiflix.Notify.info(
-            "Too many matches found. Please enter a more specific name."
-          );
-    }
-    else if(data.length > 1 || data.length < 9) {
-        insertContent(data)
-    }
-    else if (data.length === 0) {
-        Notiflix.Notify.failure('Oops, there is no country with that name');}
-    else {
+// function onSearch(e) {
+//     debounce(e => {
+//     cleanHtml();
+//     const countryName = input.value.trim();
+//     if (countryName !== '') {
+//         fetchCountries(trimmedValue).then(foundData => {  
 
-}
-})
-    .catch((error) =>{
-        console.log(error) 
-    }
+//     fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=,name,capital,population,flags,languages`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log(data);
+//     if(data.length > 10) {
+//         Notiflix.Notify.info(
+//             "Too many matches found. Please enter a more specific name."
+//           );
+//     }
+//     else if(data.length > 1 || data.length < 9) {
+//         insertContent(data)
+//     }
+//     else if (data.length === 0) {
+//         Notiflix.Notify.failure('Oops, there is no country with that name');}
+//     else {
+
+// }
+// })
+//     .catch((error) =>{
+//         console.log(error) 
+//     }
+//     )
+// }, DEBOUNCE_DELAY)
+// }
+
+// function createLi(item) {
+// `<li>
+// <img src = "${item.flag}" alt ="flag of ${item.name}" >
+// <p>${item.name}</p>
+// </li>`
+// } 
+
+// function generateContent(array) {array.reduce((acc, item) => acc + createLi(item), "")};
+
+// const insertContent = (array) => {
+// const result = generateContent(array);
+// refs.countryList.insertAdjacentHTML("beforeend", result)
+// }
+
+// function cleanHtml() {
+//     refs.countryList.innerHTML = '';
+//     refs.countryInfo.innerHTML = '';
+//   }
+
+export const fetchCountries = name => {
+    return fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
     )
-}
-
-function createLi(item) {
-`<li>
-<img src = "${item.flag}" alt ="flag of ${item.name}" >
-<p>${item.name}</p>
-</li>`
-} 
-
-function generateContent(array) {array.reduce((acc, item) => acc + createLi(item), "")};
-
-const insertContent = (array) => {
-const result = generateContent(array);
-refs.countryList.insertAdjacentHTML("beforeend", result)
-}
-
-function cleanHtml() {
-    refs.countryList.innerHTML = '';
-    refs.countryInfo.innerHTML = '';
-  }
+        .then(response => {
+        if (!response.ok) {
+          if (response.status === 404) {
+            return [];
+          }
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
