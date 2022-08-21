190322b8dd7b38eb5c5821454f10a9377cfd38b0
Alina Guscha <alialinaa19@gmail.com>
2022-08-21 23:13:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@
 //   }
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
-// import debounce from 'lodash.debounce';
+import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const input = document.querySelector('#search-box');
@@ -73,28 +73,30 @@ const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 
 input.addEventListener(
-  'input', (e => {
-      const trimmedValue = input.value.trim();
-         cleanHtml();   
-    if (trimmedValue !== '') {
-        fetchCountries(trimmedValue).then(foundData => {      
-
-        if (foundData.length > 10) {
-          Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.'
-          );
-        } else if (foundData.length === 0) {
-          Notiflix.Notify.failure('Oops, there is no country with that name');
-        } else if (foundData.length >= 2 && foundData.length <= 10) {
-         
-          renderCountryList(foundData);
-        } else if (foundData.length === 1) {
-    
-          renderOneCountry(foundData);
-        }
-      });
-    }
-  }))
+    'input',
+    debounce(e => {
+        const trimmedValue = input.value.trim();
+           cleanHtml();   
+      if (trimmedValue !== '') {
+          fetchCountries(trimmedValue).then(foundData => {      
+  
+          if (foundData.length > 10) {
+            Notiflix.Notify.info(
+              'Too many matches found. Please enter a more specific name.'
+            );
+          } else if (foundData.length === 0) {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+          } else if (foundData.length >= 2 && foundData.length <= 10) {
+           
+            renderCountryList(foundData);
+          } else if (foundData.length === 1) {
+      
+            renderOneCountry(foundData);
+          }
+        });
+      }
+    }, DEBOUNCE_DELAY)
+  );
 
 function renderCountryList(countries) {
   const markup = countries
