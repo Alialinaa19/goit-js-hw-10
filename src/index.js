7a81b2648fdec5c0d52ac61a898ac3cbaf413701
Alina Guscha <alialinaa19@gmail.com>
2022-08-21 18:37:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function onSearch(e) {
     })
     .catch((error) =>{
         console.log(error)
-        if (error.status === 404) {
+        if (error.status === "404") {
             Notiflix.Notify.failure(
                 "Oops, there is no country with that name"
               );
