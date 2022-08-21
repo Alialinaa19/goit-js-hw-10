4d9250092f00cda09898ee4affce7b07e663a35a
Alina Guscha <alialinaa19@gmail.com>
2022-08-21 22:26:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -74,30 +74,31 @@ const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 
 input.addEventListener(
-  'input', e => {
-      const trimmedValue = input.value.trim();
-         cleanHtml();   
-    if (trimmedValue !== '') {
-        fetchCountries(trimmedValue).then(foundData => {      
-
-        if (foundData.length > 10) {
-          Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.'
-          );
-        } else if (foundData.length === 0) {
-          Notiflix.Notify.failure('Oops, there is no country with that name');
-        } else if (foundData.length >= 2 && foundData.length <= 10) {
-         
-          renderCountryList(foundData);
-        } else if (foundData.length === 1) {
-    
-          renderOneCountry(foundData);
-        }
-      });
-    }
-  },
+  'input', onSearch
 );
 
+function onSearch (e) {
+    const trimmedValue = input.value.trim();
+       cleanHtml();   
+  if (trimmedValue !== '') {
+      fetchCountries(trimmedValue).then(foundData => {      
+
+      if (foundData.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (foundData.length === 0) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      } else if (foundData.length >= 2 && foundData.length <= 10) {
+       
+        renderCountryList(foundData);
+      } else if (foundData.length === 1) {
+  
+        renderOneCountry(foundData);
+      }
+    });
+  }
+},
 function renderCountryList(countries) {
   const markup = countries
     .map(country => {
